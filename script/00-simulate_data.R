9dc4b5b47b89b8f532dd6bb684feb6c9101e63d7
Brian031205 <bolin.shen@mail.utoronto.ca>
2024-03-10 23:40:00 -0400

    Initial commit

---
+++ script/00-simulate_data.R
@@ -0,0 +1,131 @@
+#### Preamble ####
+# Purpose: Simulate the scenario: Support for a political party is a binary (yes/no), and is related to age-group, gender, income group, and highest education.
+# Author: Bolin Shen
+# Date: 28 February 2024
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(rstanarm)
+library(modelsummary)
+library(ggplot2)
+library(knitr)
+
+## Creating simulated data
+
+# Set seed for reproducibility
+set.seed(42)
+
+# Sample number
+n = 1000
+
+# Simulate data, predictors associated are age group, gender, income group and education.
+simulate = tibble(
+  voted_for = sample(c("Yes", "No"), n, replace = TRUE),
+  is_support = if_else(voted_for == "Yes", 1, 0),
+  age_group = sample(c("18-24", "25-34", "35-44", "45-54", "55+"), n, replace = TRUE),
+  gender = sample(c("Male", "Female"), n, replace = TRUE),
+  income_group = sample(c("Low", "Medium", "High"), n, replace = TRUE),
+  education = sample(c("High School", "Bachelor's Degree", "Master's Degree", "Some colledge"), n, replace = TRUE)
+)
+
+
+
+# Test 1: test class
+is.character(simulate$age_group) == TRUE
+
+# Test 2: test class
+is.character(simulate$gender) == TRUE
+
+# Test 3: test class 
+is.character(simulate$income_group) == TRUE
+
+# Test 4: test class 
+is.character(simulate$education) == TRUE
+
+# Test 5: test there are 1000 observations.
+nrow(simulate) == 1000
+
+# Test 6: test class
+
+is.character(simulate$age_group) & is.character(simulate$gender)  == TRUE
+
+# Test 7: test class
+
+is.character(simulate$age_group) & is.character(simulate$education) == TRUE
+
+# Test 8: test class
+is.character(simulate$age_group) & is.character(simulate$income_group) == TRUE
+
+# Test 9: test class
+is.numeric(simulate$is_support) == TRUE
+
+# Test 10: test class
+is.character(simulate$voted_for) == TRUE
+
+
+# Build the graph
+
+sim_population <-
+  tibble(
+    edu = rbinom(n, size = 5, prob = 0.2),
+    gender = rbinom(n, size = 1, prob = 0.5),
+    probability = (edu + gender + 0.1) / 6.2, # prevent certainty
+    is_support = rbinom(n, 1, prob = probability)
+  ) 
+
+sim_population
+
+
+sample <- 
+  sim_population |> 
+  slice_sample(n = n, weight_by = probability)
+
+sample <-
+  sample |>
+  mutate(
+    is_support = if_else(is_support == 1, "Yes", "No"),
+    is_support = as_factor(is_support),
+    gender = if_else(gender == 1, "Male", "Female"),
+  )
+
+
+# Bar Plot
+sample |>
+  ggplot(aes(x = edu, fill = as_factor(is_support))) +
+  stat_count(position = "dodge") +
+  facet_wrap(facets = vars(gender)) +
+  theme_minimal() +
+  labs(
+    x = "Highest education",
+    y = "Number of respondents",
+    fill = "Is support"
+  ) +
+  coord_flip() +
+  scale_fill_brewer(palette = "Set1") +
+  theme(legend.position = "bottom")
+
+# Build the model
+political_preferences <-
+  stan_glm(
+    is_support ~ gender + education + income_group + age_group,
+    data = simulate,
+    family = binomial(link = "logit"),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = 
+      normal(location = 0, scale = 2.5, autoscale = TRUE),
+    seed = 42
+  )
+
+
+
+
+modelsummary(political_preferences)
+
+# plot model
+
+modelplot(political_preferences, conf_level = 0.9) +
+  labs(x = "90 per cent credibility interval")
+
+
+summary(political_preferences)
---
+++ script/02-data_cleaning.R
@@ -0,0 +1,85 @@
+#### Preamble ####
+# Purpose: Cleans the raw poll data
+# Author: Bolin Shen
+# Date: 21 January 2024
+# Contact: bolin.shen@mail.utoronto.ca
+# License: MIT
+
+#### Workspace setup ####
+library(tidyverse)
+library(janitor)
+library(dplyr)
+
+#### Read in raw data ####
+
+# Read in the raw polls data. 
+readr::read_csv("data/raw_poll_data.csv")
+
+#### Basic cleaning - polls data ####
+raw_poll_data <-
+  read_csv(
+    file = "data/raw_poll_data.csv",
+    show_col_types = FALSE
+  )
+
+# creating a new column to reformat the open date to year only
+data_year = as.Date(data$OPEN_DATE, format = "%Y-%m-%d")|> format("%Y")
+cleaned_poll_data = raw_poll_data |> mutate(OPEN_DATE_YEAR = data_year)
+
+
+# creating a new column to store the value of the number of ballots returned
+cleaned_poll_data$ballots_returned <- cleaned_poll_data$BALLOTS_IN_FAVOUR + cleaned_poll_data$BALLOTS_OPPOSED
+
+
+# organize name #
+cleaned_poll_data <-
+  clean_names(cleaned_poll_data)
+
+head(cleaned_poll_data)
+
+
+
+# select columns of interest #
+cleaned_poll_data <-
+  cleaned_poll_data |>
+  select(
+    application_for,
+    open_date_year,
+    response_rate_met,
+    ballots_in_favour,
+    ballots_opposed,
+    ballots_returned,
+    ballots_distributed,
+    poll_result
+  )
+
+# Filter the data by response rate met
+summarized_poll_data <- cleaned_poll_data[cleaned_poll_data$response_rate_met == "Yes", ]
+
+
+summarized_poll_data <-
+  summarized_poll_data |>
+  select(
+    open_date_year,
+    application_for,
+    ballots_in_favour,
+    ballots_distributed,
+    ballots_returned,
+    poll_result
+    
+  )
+
+
+
+# save cleaned polls data #
+write_csv(
+  x = cleaned_poll_data,
+  file = "data/cleaned_poll_data.csv"
+)
+
+# save summarized polls data #
+write_csv(
+  x = summarized_poll_data,
+  file = "data/summarized_poll_data.csv"
+)
+
